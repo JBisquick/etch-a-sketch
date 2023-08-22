8a1fc532e2830d93e0e2e5e1a7e2bd8315c9c31d
JBisquick <felipebirch@gmail.com>
2023-08-22 15:19:06 -0700

    Add slider that creates new grid

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 const grid = document.querySelector('#grid');
+const slider = document.querySelector('#slider');
+const sliderValue = document.querySelector('#slider-value');
+
+sliderValue.textContent = slider.value;
+getGrid(16);
+
+slider.oninput = function() {
+  sliderValue.textContent = this.value;
+};
 
 function getGrid(gridDimension) {
   for(let i = 0; i <= gridDimension; i++) {
@@ -7,8 +16,8 @@ function getGrid(gridDimension) {
     grid.appendChild(row);
     for(let j = 0; j <= gridDimension; j++) {
       let box = document.createElement('div');
-      box.classList.add('box')
-      row.appendChild(box)
+      box.classList.add('box');
+      row.appendChild(box);
     }
   }
 };
@@ -17,9 +26,7 @@ function getBlack(gridBox) {
   gridBox.style.backgroundColor = 'black';
 };
 
-getGrid(16);
-
-const boxes = document.querySelectorAll('.box')
+const boxes = document.querySelectorAll('.box');
 
 boxes.forEach((box) => {
   box.addEventListener('mouseover', () => {
